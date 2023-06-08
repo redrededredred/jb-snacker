6bcdd751994a6f93a3a344f06e705bdacfa6dfc0
Chad van Basedman <113964826+redrededredred@users.noreply.github.com>
2023-06-08 10:33:49 +0200

    +
    +

--- src/scrape.py
+++ src/scrape.py
@@ -1,6 +1,17 @@
 # Scraping functions here
+import datetime
+import requests
+from tabula import read_pdf
 
-hi = "Hi"
+BASE_URL = "https://www.jugendgaestehaus-johannesburg.de/fileadmin/user_upload/"
+#menu = "speisenplan_jgh_kw23.pdf"
+current_date = datetime.datetime.now()
+current_calender_week = current_date.month * 4
+menu_url = f"{BASE_URL}speisenplan_jgh_kw{current_calender_week}.pdf"
 
 if __name__ == "__main__":
   print("(+) - Running tests!")
+  data = "data.pdf"
+  with open(data, "wb") as f:
+    f.write(requests.get(menu_url).content)
+  dfs = tabula.read_pdf("data.pdf", pages='all')
